6eb5b2d89fada9b6b7a933745f036f62feccb247
John Carlo Villamar <kaizer2407@gmail.com>
2026-04-21 13:32:13 +0800

    Working version

--- app/Http/Controllers/RequestController.php
+++ app/Http/Controllers/RequestController.php
@@ -6,5 +6,12 @@ use Illuminate\Http\Request;
 
 class RequestController extends Controller
 {
-    //
+    public function resetTicket($id)
+    {
+        $req = \App\Models\RequestModel::findOrFail($id);
+        $req->tickets = null;
+        $req->save();
+
+        return back()->with('success', 'Ticket reset');
+    }
 }
